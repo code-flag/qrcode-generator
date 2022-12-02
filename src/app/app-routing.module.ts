cdb66c79bda40c3ac31bdb026fd2d37d4191a264
code-flag <awefrancolaz@gmail.com>
2022-12-02 01:13:44 +0100

    chore: netlify redirect rule added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  {path: "dashboard", component: DashboardComponent}
+  {path: "", component: DashboardComponent},
+  {path: "dashboard", component: DashboardComponent},
+  {path: "**", component: DashboardComponent}
 ];
 
 @NgModule({
